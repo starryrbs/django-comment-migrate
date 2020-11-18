3921fe86581981d3d1da77cc322d4e621605b877
starryrbs <1322096624@qq.com>
2020-11-18 15:03:09 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-comment-migrate',
+    version='0.0.1',
+    packages=['tests', 'django_comment_migrate'],
+    url='',
+    license='',
+    author='starryrbs',
+    author_email='1322096624@qq.com',
+    description=''
+)
