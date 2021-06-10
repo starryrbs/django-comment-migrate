43660b2769254e4ffa3d5d33ec960186d11c689a
Yusuke MURAOKA <yusuke@jbking.org>
2021-06-10 14:01:17 +0900

    treat auth.User when AUTH_USER_MODEL is specified for another

--- django_comment_migrate/apps.py
+++ django_comment_migrate/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig, apps
+from django.contrib.auth import get_user_model
 from django.db import DEFAULT_DB_ALIAS
 from django.db.models.signals import post_migrate
 
@@ -7,8 +8,13 @@ from django_comment_migrate.utils import get_migrations_app_models
 
 
 def handle_post_migrate(app_config, using=DEFAULT_DB_ALIAS, **kwargs):
+    from django.contrib.auth.models import User
+
     migrations = (migration for migration, rollback in kwargs.get('plan', []) if not rollback)
     app_models = get_migrations_app_models(migrations, apps, using)
+    # another user model is specified instead.
+    if get_user_model() != User:
+        app_models -= {User}
     migrate_app_models_help_text_to_database(app_models, using)
 
 
